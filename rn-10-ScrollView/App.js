73a6b1aa56d0c3edabeb8c5b7e7ce1b8672501d6
KyoungRan <kyoungrankang@gmail.com>
2017-08-28 02:56:01 +0900

    10 ScrollView

--- rn-10-ScrollView/App.js
+++ rn-10-ScrollView/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <ScrollView>
-        <Text style={{
-          fontSize: 30,
-          padding: 20,
-          backgroundColor: "red"
-        }}>
-        This is a text a ScrollView
+      <ScrollView horizontal={true}>
+        <Text
+          style={{
+            fontSize: 30,
+            padding: 20,
+            backgroundColor: 'red'
+          }}
+        >
+          This is a text a ScrollView
         </Text>
-        <Text style={{
-          fontSize: 30,
-          padding: 20,
-          backgroundColor: "green"
-        }}>
-        This is second Line
+        <Text
+          style={{
+            fontSize: 30,
+            padding: 20,
+            backgroundColor: 'green'
+          }}
+        >
+          This is second Line
+        </Text>
+        <Image
+          source={require('./images/CSNLCbw.jpg')}
+          style={{ width: 414, height: 414 }}
+        />
+        <Text style={{ fontSize: 30, padding: 20, backgroundColor: 'yellow' }}>
+          This is 4th line
+        </Text>
+        <Image
+          source={require('./images/YvHYBBB.jpg')}
+          style={{ width: 414, height: 414 }}
+        />
+        <Text style={{ fontSize: 30, padding: 20, backgroundColor: 'blue' }}>
+          This is 5th line
         </Text>
       </ScrollView>
-    )
+    );
   }
 }
