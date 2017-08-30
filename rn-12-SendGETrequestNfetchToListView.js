486a1a4f7d356034b4c4327728cfdcb59321bc17
KyoungRan <kyoungrankang@gmail.com>
2017-08-30 23:49:52 +0900

    12-Send GET request and fetch to ListView

--- rn-12-SendGETrequestNfetchToListView.js
+++ rn-12-SendGETrequestNfetchToListView.js
@@ -3,32 +3,59 @@ import { ActivityIndicator, ListView, Text, View } from 'react-native';
 
 export default class MoviesComponent extends Component {
   state = {
+    isLoading: true,
     clonedMovies: []
   };
 
   componentDidMount() {
     fetch('https://facebook.github.io/react-native/movies.json')
-      .then(response => response.josn())
+      .then(response => response.json())
       .then(responseJson => {
-        let standardDataSource = new ListView.DataSource({
+        const standardDataSource = new ListView.DataSource({
           rowHasChanged: (r1, r2) => r1 !== r2
         });
-        rhis.setState({
+        /*
+    const movies = [
+      {
+        title: 'Star Wars',
+        releaseYear: '1977'
+      },
+      {
+        title: 'The Matrix',
+        releaseYear: '1999'
+      }
+    ];
+    */
+        // this.state.clonedMovies = standardDataSource.cloneWithRows(movies);
+        this.setState({
+          isLoading: false,
           clonedMovies: standardDataSource.cloneWithRows(responseJson.movies)
         });
       });
   }
+
   render() {
+    console.log('1. Getting data');
+    if (this.state.isLoading) {
+      return (
+        <View>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+    console.log('2. Already get data');
     return (
       <View style={{ flex: 1, paddingTop: 30 }}>
         <ListView
           dataSource={clonedMovies}
-          renderRow={rowData =>
+          renderRow={rowData => (
             <Text>
               Title: {rowData.title}, release year: {rowData.releaseYear}
-            </Text>}
+            </Text>
+          )}
         />
       </View>
     );
   }
 }
+
